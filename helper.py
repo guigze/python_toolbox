f67eb1135b00e4324ba8f2677c905e9275d9bcdb
guigze <116827840+guigze@users.noreply.github.com>
2022-10-28 13:04:54 +0200

    Update helper.py

--- helper.py
+++ helper.py
@@ -7,18 +7,22 @@ def plot_curves(history):
   accuracy = history.history['accuracy']
   val_accuracy = history.history['val_accuracy']
 
-  epochs = range(len(history.history(['loss']))
+  epochs = range(len(history.history['loss']))
+
+  plt. figure(figsize = (20,7))
 
   # Plot loss
-  plt.plot(epochs, loss, label = 'Training loss')
-  plt.plot(epochs, val_loss, label = 'Validation loss')
+  plt.subplot(1,2,1)
+  plt.plot(epochs, loss, label = 'Training loss', c = 'b')
+  plt.plot(epochs, val_loss, label = 'Validation loss', c = 'r')
   plt.title('Loss')
   plt.xlabel('Epochs')
   plt.legend();
 
   # Plot accuracy
-  plt.plot(epochs, accuracy, label = 'Training accuracy')
-  plt.plot(epochs, val_accuracy, label = 'Validation accuracy')
+  plt.subplot(1,2,2)
+  plt.plot(epochs, accuracy, label = 'Training accuracy', c = 'b')
+  plt.plot(epochs, val_accuracy, label = 'Validation accuracy', c = 'r')
   plt.title('Accuracy')
   plt.xlabel('Epochs')
   plt.legend();
