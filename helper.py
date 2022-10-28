a1cc0ab391145a332236d7f752894f0cbd47ad79
guigze <116827840+guigze@users.noreply.github.com>
2022-10-28 16:20:34 +0200

    Update helper.py

--- helper.py
+++ helper.py
@@ -1,11 +1,17 @@
 import matplotlib.pyplot as plt
 
-def plot_curves(history):
+def plot_curves(history, new_history = None, fine_tuning = False, initial_epochs = None):
 
   loss = history.history['loss']
   val_loss = history.history['val_loss']
   accuracy = history.history['accuracy']
   val_accuracy = history.history['val_accuracy']
+ 
+  if fine_tuning:
+    loss += new_history.history['loss']
+    val_loss += new_history.history['val_loss']
+    accuracy += new_history.history['accuracy']
+    val_accuracy += new_history.history['val_accuracy']
 
   epochs = range(len(history.history['loss']))
 
@@ -13,16 +19,23 @@ def plot_curves(history):
 
   # Plot loss
   plt.subplot(1,2,1)
-  plt.plot(epochs, loss, label = 'Training loss', c = 'b')
-  plt.plot(epochs, val_loss, label = 'Validation loss', c = 'r')
+  plt.plot(epochs, loss, label = 'Training', c = 'b')
+  plt.plot(epochs, val_loss, label = 'Validation', c = 'r')
   plt.title('Loss')
   plt.xlabel('Epochs')
+  if fine_tuning:
+    plt.plot([initial_epochs, initial_epochs],
+              plt.ylim(), label='Fine Tuning', c = 'g')
   plt.legend();
+
 
   # Plot accuracy
   plt.subplot(1,2,2)
-  plt.plot(epochs, accuracy, label = 'Training accuracy', c = 'b')
-  plt.plot(epochs, val_accuracy, label = 'Validation accuracy', c = 'r')
+  plt.plot(epochs, accuracy, label = 'Training', c = 'b')
+  plt.plot(epochs, val_accuracy, label = 'Validation', c = 'r')
   plt.title('Accuracy')
   plt.xlabel('Epochs')
+  if fine_tuning:
+    plt.plot([initial_epochs, initial_epochs],
+              plt.ylim(), label='Fine Tuning', c = 'g')
   plt.legend();
