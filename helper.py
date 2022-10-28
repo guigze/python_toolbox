0dd06aba522d26fbc4b7b7f4619257b2665a5e55
guigze <116827840+guigze@users.noreply.github.com>
2022-10-28 12:56:46 +0200

    Update helper.py

--- helper.py
+++ helper.py
@@ -2,23 +2,23 @@ import matplotlib.pyplot as plt
 
 def plot_curves(history):
 
-loss = history.history['loss']
-val_loss = history.history['val_loss']
-accuracy = history.history['accuracy']
-val_accuracy = history.history['val_accuracy']
+  loss = history.history['loss']
+  val_loss = history.history['val_loss']
+  accuracy = history.history['accuracy']
+  val_accuracy = history.history['val_accuracy']
 
-epochs = range(len(history.history(['loss']))
+  epochs = range(len(history.history(['loss']))
 
-# Plot loss
-plt.plot(epochs, loss, label = 'Training loss')
-plt.plot(epochs, val_loss, label = 'Validation loss')
-plt.title('Loss')
-plt.xlabel('Epochs')
-plt.legend();
+  # Plot loss
+  plt.plot(epochs, loss, label = 'Training loss')
+  plt.plot(epochs, val_loss, label = 'Validation loss')
+  plt.title('Loss')
+  plt.xlabel('Epochs')
+  plt.legend();
 
-# Plot accuracy
-plt.plot(epochs, accuracy, label = 'Training accuracy')
-plt.plot(epochs, val_accuracy, label = 'Validation accuracy')
-plt.title('Accuracy')
-plt.xlabel('Epochs')
-plt.legend();
+  # Plot accuracy
+  plt.plot(epochs, accuracy, label = 'Training accuracy')
+  plt.plot(epochs, val_accuracy, label = 'Validation accuracy')
+  plt.title('Accuracy')
+  plt.xlabel('Epochs')
+  plt.legend();
